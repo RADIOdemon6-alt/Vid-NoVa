2e0e21a374291fe40abd33abc42bcc8983e80c82
RADIOdemon6-alt <abyzaryws@gmail.com>
2025-07-25 16:21:55 +0300

    Update script.js

--- assets/pages/home/script.js
+++ assets/pages/home/script.js
@@ -1,1 +1,25 @@
+const uploadBtn = document.getElementById('upload-btn');
+const uploadOptions = document.getElementById('upload-options');
+const settingsBtn = document.getElementById('settings-btn');
+const settingsPopup = document.getElementById('settings-popup');
+const overlay = document.getElementById('overlay');
+const changeLang = document.getElementById('change-lang');
 
+uploadBtn.addEventListener('click', () => {
+  uploadOptions.style.display = uploadOptions.style.display === 'block' ? 'none' : 'block';
+  uploadOptions.classList.toggle('active');
+});
+
+settingsBtn.addEventListener('click', () => {
+  settingsPopup.classList.add('active');
+  overlay.style.display = 'block';
+});
+
+overlay.addEventListener('click', () => {
+  settingsPopup.classList.remove('active');
+  overlay.style.display = 'none';
+});
+
+changeLang.addEventListener('click', () => {
+  window.location.href = 'https://translate.google.com/translate?hl=&sl=auto&tl=auto&u=' + window.location.href;
+});
